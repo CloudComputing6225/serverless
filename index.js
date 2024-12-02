0160653883ad7aa05c8e8235ab46ad020ca64044
Saurabh Srivastava <srivastava.sau@northeastern.edu>
2024-12-02 11:20:34 -0800

    Adding Secrets

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 import mailgun from 'mailgun-js';
 import AWS from 'aws-sdk';
 
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN
-});
+const secretsManager = new AWS.SecretsManager();
+
+async function getMailgunApiKey() {
+  const secretName = process.env.MAILGUN_API_KEY;
+  console.log('Secret Name:', secretName);
+  try {
+    const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+    if ('SecretString' in data) {
+      const secretValue = data.SecretString;
+      // Check if the secret is already a JSON object
+      try {
+        const secret = JSON.parse(secretValue);
+        return secret.api_key;
+      } catch (parseError) {
+        // If parsing fails, assume the secret is the API key itself
+        return secretValue;
+      }
+    } else if ('SecretBinary' in data) {
+      // Handle binary secret (if applicable)
+      const buff = Buffer.from(data.SecretBinary, 'base64');
+      return buff.toString('ascii');
+    } else {
+      throw new Error('Secret not found in SecretString or SecretBinary');
+    }
+  } catch (error) {
+    console.error('Error retrieving Mailgun API key:', error);
+    throw error;
+  }
+}
 
 export const handler = async (event) => {
   console.log('Lambda function invoked with event:', JSON.stringify(event));
 
   try {
+    const apiKey = await getMailgunApiKey();
+    const domain = 'saurabhprojects.me';
+
+    const mg = mailgun({ apiKey, domain });
     const message = JSON.parse(event.Records[0].Sns.Message);
     console.log('Parsed SNS message:', message);
 
     const { userId, email, firstName, verificationToken } = message;
 
-    // Send verification email
-    const verificationLink = `http://dev.saurabhprojects.me/v2/verify?token=${verificationToken}`;
+    const verificationLink = `https://dev.saurabhprojects.me/v2/verify?token=${verificationToken}`;
+    console.log(verificationLink);
+    console.log(email);
     const data = {
-      from: 'noreply@saurabhprojects.me',
+      from: `noreply@saurabhprojects.me`,
       to: email,
       subject: 'Verify Your Email',
       text: `Hello ${firstName},\n\nPlease verify your email by clicking this link: ${verificationLink}\nThis link will expire in 2 minutes.`,
